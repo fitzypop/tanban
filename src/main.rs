f8b5bfc3098f114914d4d6c12df42a0fc568abad
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-11-23 15:40:55 -0700

    start to get a feel for this

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,75 @@
-use chrono::{DateTime, Utc};
-use crossterm::terminal::enable_raw_mode;
-use std::io;
-use tui::{backend::CrosstermBackend, Terminal};
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen, SetTitle,
+    },
+    ExecutableCommand,
+};
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders},
+    Terminal,
+};
 
-#[derive(Debug, Clone)]
-enum Status {
-    Todo,
-    InProgress,
-    Done,
-}
+// use chrono::{DateTime, Utc};
 
-impl Default for Status {
-    fn default() -> Self {
-        Self::Todo
-    }
-}
+// #[derive(Debug, Default, Clone)]
+// enum Status {
+//     #[default]
+//     Todo,
+//     InProgress,
+//     Done,
+// }
 
-#[derive(Debug, Default, Clone)]
-struct Card {
-    title: String,
-    description: String,
-    created: DateTime<Utc>,
-    status: Status,
-}
+// #[derive(Debug, Default, Clone)]
+// struct Card {
+//     title: String,
+//     description: String,
+//     created: DateTime<Utc>,
+//     status: Status,
+// }
 
-#[derive(Debug, Default)]
-struct AppState {
-    cards: Vec<Card>,
-}
+// #[derive(Debug, Default)]
+// struct AppState {
+//     cards: Vec<Card>,
+// }
 
-impl AppState {
-    pub fn new() -> Self {
-        Self {
-            cards: vec![Card::default(); 2],
-        }
-    }
-}
+// impl AppState {
+//     pub fn new() -> Self {
+//         Self {
+//             cards: vec![Card::default(); 2],
+//         }
+//     }
+// }
+
 fn main() -> Result<(), io::Error> {
-    enable_raw_mode().unwrap();
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let terminal = Terminal::new(backend)?;
+    // setup terminal
+    enable_raw_mode()?;
 
-    // idk wtf im doing, and im tired af
-    let state = AppState::new();
-    println!("{:?}", state);
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let mut backend = CrosstermBackend::new(stdout);
+    backend.execute(SetTitle("Tanban - Rust Kanban Board"))?;
+
+    let mut terminal = Terminal::new(backend)?;
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default().title("Block").borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
     Ok(())
 }
