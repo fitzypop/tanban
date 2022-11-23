9fea2c13a2beb04a88e3d4406e94f73dd386d6d7
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-11-23 14:56:36 -0700

    idk man, coding while sick and with a sick kiddo is hard

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,49 @@
-#[allow(unused_variables)]
+use chrono::{DateTime, Utc};
+use crossterm::terminal::enable_raw_mode;
 use std::io;
 use tui::{backend::CrosstermBackend, Terminal};
 
+#[derive(Debug, Clone)]
+enum Status {
+    Todo,
+    InProgress,
+    Done,
+}
+
+impl Default for Status {
+    fn default() -> Self {
+        Self::Todo
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+struct Card {
+    title: String,
+    description: String,
+    created: DateTime<Utc>,
+    status: Status,
+}
+
+#[derive(Debug, Default)]
+struct AppState {
+    cards: Vec<Card>,
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        Self {
+            cards: vec![Card::default(); 2],
+        }
+    }
+}
 fn main() -> Result<(), io::Error> {
+    enable_raw_mode().unwrap();
     let stdout = io::stdout();
     let backend = CrosstermBackend::new(stdout);
-    let _terminal = Terminal::new(backend)?;
+    let terminal = Terminal::new(backend)?;
+
+    // idk wtf im doing, and im tired af
+    let state = AppState::new();
+    println!("{:?}", state);
     Ok(())
 }
