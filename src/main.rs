a2ab6166f9784747c2d85e6ee1a3614406426afd
fitzypop <32967490+fitzypop@users.noreply.github.com>
2022-11-28 23:42:44 -0700

    created basic cursive app and included easy themeing configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use cursive::views::{Dialog, TextView};
+
 fn main() {
-    todo!()
-    // implement cursive tui
+    let mut siv = cursive::default();
+
+    // set themeing via toml tile
+    siv.load_toml(include_str!("theme.toml")).unwrap();
+
+    siv.add_layer(
+        Dialog::around(TextView::new("Hello TUI!!"))
+            .title("Tanban")
+            .button("Quit", |s| s.quit()),
+    );
+
+    siv.run();
 }
